f78be51e553603bf5c13e29baa1e08acd563f140
saurabh-shete <ss.saurabhshete@gmail.com>
2023-09-15 00:42:26 +0530

    classes and functions are declared

---
+++ assessment.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Coordinates
+{
+  int x, y, z;
+};
+
+enum Direction
+{
+  N,
+  S,
+  E,
+  W,
+  Up,
+  Down
+};
+
+class Spacecraft
+{
+public:
+  Spacecraft(int x, int y, int z, Direction initialDirection)
+      : coordinates({x, y, z}), direction(initialDirection) {}
+
+  void move(char command)
+  {
+    switch (command)
+    {
+    case 'f':
+      moveForward();
+      break;
+    case 'b':
+      moveBackward();
+      break;
+    case 'l':
+      turnLeft();
+      break;
+    case 'r':
+      turnRight();
+      break;
+    case 'u':
+      turnUp();
+      break;
+    case 'd':
+      turnDown();
+      break;
+    }
+  }
+
+  void printFinalPosition() const
+  {
+    cout << "Final Position: (" << coordinates.x << ", " << coordinates.y << ", " << coordinates.z << ")\n";
+  }
+
+  void printFinalDirection() const
+  {
+    const char *directionNames[] = {"N", "S", "E", "W", "Up", "Down"};
+    cout << "Final Direction: " << directionNames[direction] << "\n";
+  }
+
+  int getX() const
+  {
+    return coordinates.x;
+  }
+
+  int getY() const
+  {
+    return coordinates.y;
+  }
+
+  int getZ() const
+  {
+    return coordinates.z;
+  }
+
+  Direction getDirection() const
+  {
+    return direction;
+  }
+
+private:
+  Coordinates coordinates;
+  Direction direction;
+  Direction prevDirection;
+  int countUpDown = 0;
+};
