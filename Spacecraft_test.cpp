c6987c244f7604a263d21788ccbdd2364553fe5a
saurabh-shete <ss.saurabhshete@gmail.com>
2023-09-15 00:01:19 +0530

    main file to be executed initialized

---
+++ Spacecraft_test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdexcept>
+#include "assement"
+
+void testInitialPosition()
+{
+  Spacecraft spacecraft(0, 0, 0, Direction::N);
+
+  if (spacecraft.getX() == 0 &&
+      spacecraft.getY() == 0 &&
+      spacecraft.getZ() == 0 &&
+      spacecraft.getDirection() == Direction::N)
+  {
+    std::cout << "Initial Position Test: Passed\n";
+  }
+  else
+  {
+    std::cout << "Initial Position Test: Failed\n";
+  }
+}
+
+int main()
+{
+
+  testInitialPosition();
+  return 0;
+}
