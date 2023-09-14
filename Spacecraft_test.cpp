c7fd4544035fb27e40477da240eaf0005a275ec8
saurabh-shete <ss.saurabhshete@gmail.com>
2023-09-15 00:06:37 +0530

    move forward and backward test case added

--- Spacecraft_test.cpp
+++ Spacecraft_test.cpp
@@ -19,9 +19,48 @@ void testInitialPosition()
   }
 }
 
+void testMoveForward()
+{
+  Spacecraft spacecraft(0, 0, 0, Direction::N);
+  spacecraft.move('f');
+
+  if (spacecraft.getX() == 0 &&
+      spacecraft.getY() == 1 &&
+      spacecraft.getZ() == 0 &&
+      spacecraft.getDirection() == Direction::N)
+  {
+    std::cout << "Move Forward Test: Passed\n";
+  }
+  else
+  {
+    std::cout << "Move Forward Test: Failed\n";
+  }
+}
+
+void testMoveBackward()
+{
+  Spacecraft spacecraft(0, 0, 0, Direction::N);
+  spacecraft.move('b');
+
+  if (spacecraft.getX() == 0 &&
+      spacecraft.getY() == -1 &&
+      spacecraft.getZ() == 0 &&
+      spacecraft.getDirection() == Direction::N)
+  {
+    std::cout << "Move Backward Test: Passed\n";
+  }
+  else
+  {
+    std::cout << "Move Backward Test: Failed\n";
+  }
+}
+
 int main()
 {
 
   testInitialPosition();
+  testMoveForward();
+  testMoveBackward();
+
   return 0;
 }
